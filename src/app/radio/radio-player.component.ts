d35a110609e4da98e44445b66f5b462f2c7f27a3
Michael Yakhnin <mikelikesnepling@gmail.com>
2025-03-03 14:53:35 +0200

    updates

--- src/app/radio/radio-player.component.ts
+++ src/app/radio/radio-player.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit} from "@angular/core";
-import { RadioSt} from '../radio';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import RadioStIsr from '../isr.json';
+import RadioStUkr from '../ukr.json';
+
+interface StationImage {
+  path: string;
+  id: number;
+}
 
 @Component({
   selector: "app-radio-player",
@@ -9,32 +17,58 @@ import { RadioSt} from '../radio';
 
 export class RadioPlayerComponent implements OnInit {
 
-  radioStations = RadioSt;
+  radioStations:{id:string, url:string, name: string}[] = RadioStIsr;
   src: any;
-  stImages: any[] = [];
+  stImages: StationImage[] = [];
+  private queryParamSubscription: Subscription | undefined;
+  routeParam: string | null = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-
-   let arr = localStorage.getItem('stations');
-   if(arr){
-    this.stImages = JSON.parse(arr);
-   }
+    this.queryParamSubscription = this.route.queryParams.subscribe((params) => {
+      this.routeParam = params['country']; 
+      if(this.routeParam === 'isr'){
+        this.radioStations = RadioStIsr;
+      } else if(this.routeParam === 'ukr'){
+        this.radioStations = RadioStUkr;
+      }
+      else{
+        this.routeParam = 'isr';
+      }
+      const savedStations = localStorage.getItem('stations'+this.routeParam);
+      if (savedStations) {
+        this.stImages=JSON.parse(savedStations);
+      }
+      else{
+        this.stImages=[];
+      }
+    });
+   
   }
 
   play(id):void {
-    this.src = '';
-    this.src = this.getSrcById(id);
-
-    //setTimeout(() => this.src = this.getSrcById(id), 50);
+    setTimeout(() => this.src = this.getSrcById(id), 50);
   }
-  getSrcById(id): any {
+  getSrcById(id: number): any {
     return this.radioStations[id];
   }
-  addToFavorities(id): void {
-    this.stImages.push({path: 'assets/images/small/' + this.getSrcById(id)[0] + '.jpg', id: id});
-    localStorage.setItem('stations',JSON.stringify(this.stImages));
+  // addToFavorities(id): void {
+  //   this.stImages.push({path: 'assets/images/small/' + this.getSrcById(id).id + '.jpg', id: id});
+  //   localStorage.setItem('stations',JSON.stringify(this.stImages));
+  // }
+  addToFavorities(id: number): void {
+    const newStation: StationImage = {
+      path: `assets/${this.routeParam}/${this.getSrcById(id).id}${this.routeParam=='isr'?'.jpg':'.png'}`,
+      id: id
+    };
+    this.stImages.push(newStation);
+    localStorage.setItem('stations'+this.routeParam, JSON.stringify(this.stImages));
+  }
+  ngOnDestroy(): void {
+    if (this.queryParamSubscription) {
+      this.queryParamSubscription.unsubscribe();
+    }
   }
 }
 
